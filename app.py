be24e8d8b25f8710691c20ebad416604f902bfb5
Nataliia Zvarych <nataliia.zvarych@students.fhnw.ch>
2025-11-19 22:07:02 +0100

    Update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from modules.ui import display_menu, get_menu_choice, get_workout_type_choice, get_duration, get_date
 from modules.storage import save_workout
+from modules.workout import edit_workout
+from modules.history import show_history
+
 
 def main():
     """Main program loop"""
@@ -26,7 +29,7 @@ def main():
         elif choice == "2":
             # Mutate/Update workout
             print("\n--- Workout mutieren ---")
-            print("(Diese Funktion kommt später)")
+            edit_workout()
 
         elif choice == "3":
             # Delete workout
@@ -36,7 +39,7 @@ def main():
         elif choice == "4":
             # View history
             print("\n--- Historie einsehen ---")
-            print("(Diese Funktion kommt später)")
+            show_history()
 
         elif choice == "0":
             print("\nAuf Wiedersehen!")
